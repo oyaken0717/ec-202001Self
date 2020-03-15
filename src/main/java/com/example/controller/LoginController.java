e65f6718059e1a0e6fb2337dbae84884a03a8b51
KenjiOyamada <oyaken0717@yahoo.co.jp>
2020-03-15 14:28:26 +0900

    【new】LoginControllerクラス作成

---
+++ src/main/java/com/example/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
+public class LoginController {
+	
+	@RequestMapping("/to-login")
+	public String login() {
+		return "login";
+	}
+}
