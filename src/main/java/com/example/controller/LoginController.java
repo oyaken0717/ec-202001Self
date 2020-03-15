26f1b2bbb64f11f99d0e7f0137ad103035f563f1
KenjiOyamada <oyaken0717@yahoo.co.jp>
2020-03-15 15:36:30 +0900

    【new】Userドメインクラスの作成

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -3,10 +3,20 @@ package com.example.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * ログインする時のコントローラー.
+ * 
+ * @author oyamadakenji
+ *
+ */
 @Controller
 @RequestMapping("")
 public class LoginController {
 	
+	/**
+	 * ログイン画面へ
+	 * @return ログイン画面
+	 */
 	@RequestMapping("/to-login")
 	public String login() {
 		return "login";
--- src/main/java/com/example/controller/RegisterUserContorller.java
+++ src/main/java/com/example/controller/RegisterUserContorller.java
@@ -3,10 +3,21 @@ package com.example.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * ユーザー登録をする時のコントローラー.
+ * 
+ * @author oyamadakenji
+ *
+ */
 @Controller
 @RequestMapping("/register-user")
 public class RegisterUserContorller {
 	
+	/**
+	 * ユーザー登録画面へ
+	 * 
+	 * @return ユーザー登録画面
+	 */
 	@RequestMapping("/to-register")
 	public String toRegister() {
 		return "register_user";
