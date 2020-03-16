574896fabdec1d0606703942bd5054ea69a96c3a
KenjiOyamada <oyaken0717@yahoo.co.jp>
2020-03-16 18:47:12 +0900

    【modify】ItemListControllerクラスのshowItemListメソッドに 　　　　　商品一覧の情報をモデルに格納できるようにコードを追加

--- src/main/java/com/example/controller/ItemListController.java
+++ src/main/java/com/example/controller/ItemListController.java
@@ -1,14 +1,32 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Item;
+import com.example.service.ItemService;
 
 @Controller
 @RequestMapping("")
 public class ItemListController {
 
+	@Autowired
+	private ItemService itemService; 
+	
+	/**
+	 * 商品一覧画面へ.
+	 * 
+	 * @param model 商品一覧情報が入った「itemList」が入る予定
+	 * @return 商品一覧画面
+	 */
 	@RequestMapping("")
-	public String showItemList() {
+	public String showItemList(Model model) {
+		List<Item> itemList = itemService.findAll();
+		model.addAttribute("itemList", itemList);
 		return "item_list_noodle";
 	}
 }
