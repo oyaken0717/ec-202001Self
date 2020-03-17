bd0e7158b7bddea96b9105c29214a8b6c54cb91c
KenjiOyamada <oyaken0717@yahoo.co.jp>
2020-03-17 13:10:19 +0900

    【new】ItemDetailControllerクラスの作成

--- src/main/java/com/example/controller/ItemDetailController.java
+++ src/main/java/com/example/controller/ItemDetailController.java
@@ -1,14 +1,25 @@
 package com.example.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.domain.Item;
+import com.example.domain.Topping;
 import com.example.service.ItemService;
 import com.example.service.ToppingService;
 
+/**
+ * 商品詳細画面に関するコントローラー.
+ * 
+ * @author oyamadakenji
+ *
+ */
 @Controller
-@RequestMapping("item-detail")
+@RequestMapping("/item-detail")
 public class ItemDetailController {
 
 	@Autowired
@@ -17,5 +28,19 @@ public class ItemDetailController {
 	@Autowired
 	public ToppingService toppingService;
 	
-	
+	/**
+	 * 商品詳細画面にいく.
+	 * 
+	 * @param id 商品一覧から流れてくるid
+	 * @param model モデル
+	 * @return 商品詳細画面へ
+	 */
+	@RequestMapping("/show-detail")
+	public String showDetail(Integer id, Model model) {
+		Item item = itemService.load(id);
+		List<Topping> toppingList = toppingService.findAll();
+		item.setToppingList(toppingList);
+		model.addAttribute("item", item);
+		return "item_detail";
+	}	
 }
