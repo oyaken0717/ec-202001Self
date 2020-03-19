a1e0132a500729dcc8177888f10f1bdee198d3d1
KenjiOyamada <oyaken0717@yahoo.co.jp>
2020-03-19 12:56:48 +0900

    【new】CartFormクラスの作成

---
+++ src/main/java/com/example/form/CartForm.java
@@ -0,0 +1,51 @@
+package com.example.form;
+
+import java.util.List;
+
+/**
+ * カートに入れる商品情報のドメイン
+ * 
+ * @author oyamadakenji
+ *
+ */
+public class CartForm {
+	
+	/** 商品ID(ラーメン) */
+	private Integer itemId;
+	/** サイズ */
+	private String size;
+	/** 選択したトッピング */
+	private List<Integer> orderToppingList;
+	/** 数量 */
+	private Integer quantity;
+	
+	public Integer getItemId() {
+		return itemId;
+	}
+	public void setItemId(Integer itemId) {
+		this.itemId = itemId;
+	}
+	public String getSize() {
+		return size;
+	}
+	public void setSize(String size) {
+		this.size = size;
+	}
+	public List<Integer> getOrderToppingList() {
+		return orderToppingList;
+	}
+	public void setOrderToppingList(List<Integer> orderToppingList) {
+		this.orderToppingList = orderToppingList;
+	}
+	public Integer getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	@Override
+	public String toString() {
+		return "CartForm [itemId=" + itemId + ", size=" + size + ", orderToppingList=" + orderToppingList
+				+ ", quantity=" + quantity + "]";
+	}	
+}
