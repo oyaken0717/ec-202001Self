7c20e518d646f2143189935c05c77d90822cfdda
KenjiOyamada <oyaken0717@yahoo.co.jp>
2020-03-24 15:06:37 +0900

    【new】注文確認画面を表示する。

--- src/main/java/com/example/controller/OrderController.java
+++ src/main/java/com/example/controller/OrderController.java
@@ -1,11 +1,16 @@
 package com.example.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.domain.LoginUser;
 import com.example.domain.Order;
-import com.example.service.OrderService;
+import com.example.service.CartService;
 
 /**
  * カートに入れた注文情報どうのこうのする.
@@ -18,12 +23,20 @@ import com.example.service.OrderService;
 public class OrderController {
 
 	@Autowired
-	private OrderService orderService;
+	private HttpSession session;
+	@Autowired
+	private CartService cartService;
 	
 	@RequestMapping("/to-order-confirm")
-	public String toOrderConfirm() {
-//		Order order=orderService.findByUserIdAndStatus(userId,status);
-//		Order order = orderService.
+	public String toOrderConfirm(@AuthenticationPrincipal LoginUser loginUser, Model model) {
+		Integer userId = session.getId().hashCode();
+		if (loginUser != null) {
+			userId = loginUser.getUser().getId();
+		}
+		//■引数の「0」はstatus		
+		Order order = cartService.showCartList(userId, 0);
+		model.addAttribute("orderItemList", order.getOrderItemList());
+		model.addAttribute("order", order);
 		return "order_confirm";
 	}
 }
